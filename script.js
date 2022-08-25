41ca78c709c70fffdf3ca94201ce8e117329c17e
Ricardo Gallozzi <92772483+RicJr06@users.noreply.github.com>
2022-08-25 19:36:45 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function desenho() {
+    escolha = document.querySelector("#escolha")
+    escolha.src = "https://sketch.io/sketchpad/"
+}
+function audio(){
+    escolha = document.querySelector("#escolha")
+    escolha.src = "https://dictation.io/speech"
+}
